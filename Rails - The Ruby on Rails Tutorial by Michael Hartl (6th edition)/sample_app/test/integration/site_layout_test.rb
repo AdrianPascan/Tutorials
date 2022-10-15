dc8a3a1dcf7aaff3b48ddad77d537f5c21caf77b
Adrian P <pascan.adrian.08@gmail.com>
2022-10-15 16:41:54 +0300

    Add sample_app for The Ruby on Rails Tutorial

---
+++ Rails - The Ruby on Rails Tutorial by Michael Hartl (6th edition)/sample_app/test/integration/site_layout_test.rb
@@ -0,0 +1,37 @@
+require "test_helper"
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:michael)
+  end
+
+  test "layout links" do
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", logout_path, count: 0
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", signup_path
+
+    get help_path
+    assert_select "title", full_title("Help")
+    get login_path
+    assert_select "title", full_title("Log in")
+    get about_path
+    assert_select "title", full_title("About")
+    get contact_path
+    assert_select "title", full_title("Contact")
+    get signup_path
+    assert_select "title", full_title("Sign up")
+
+    log_in_as(@user)
+    get root_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user)
+    assert_select "a[href=?]", logout_path
+  end
+end
